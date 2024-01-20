1e8e45921dddaa6a7a5c7da3b0ee43786712d90b
Edmond Lin <edmondlinn@gmail.com>
2024-01-19 21:33:13 -0800

    Fix issues with menus

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, HStack, Show} from "@chakra-ui/react"
+import { Box, Flex, Grid, GridItem, HStack, Show} from "@chakra-ui/react"
 
 export interface GameQuery {
   genre: Genre | null;
@@ -32,11 +32,13 @@ function App() {
     </GridItem>
     </Show>
     <GridItem area='main' bg='navyblue'>
-      <HStack spacing={5} paddingLeft={2} marginBottom={5}>
-        <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) => setGameQuery({ ...gameQuery, platform})}/>
+      <Flex paddingLeft={2} marginBottom={5}>
+        <Box marginRight={5}>
+          <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) => setGameQuery({ ...gameQuery, platform})}/>
+        </Box>
         <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder)=> setGameQuery({
           ...gameQuery, sortOrder })}/>
-      </HStack>
+      </Flex>
       <GameGrid gameQuery={gameQuery} />
     </GridItem>
   </Grid>
--- src/hooks/useGames.tsx
+++ src/hooks/useGames.tsx
@@ -20,7 +20,7 @@ export interface Game {
 }
 
 const useGames = (gameQuery: GameQuery) => useData<Game>('/games', { params: { genres: gameQuery.genre?.id, 
-        platforms: gameQuery.genre?.id,
+        platforms: gameQuery.platform?.id,
         ordering: gameQuery.sortOrder
     }}, 
     [gameQuery]);
